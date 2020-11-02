b845ff0ab51fba77a9272fc9eb3d0fecd3451638
tân nguyễn <nguyentan2212@gmail.com>
2020-11-02 12:34:48 +0700

    add small Mario animation

--- SuperMario/AnimationManager.cpp
+++ SuperMario/AnimationManager.cpp
@@ -11,73 +11,117 @@ AnimationManager* AnimationManager::GetInstance()
 	return instance;
 }
 
-void AnimationManager::RenderAnimation(int id, float x, float y)
-{
-	animations[id]->Render(x, y);
-}
-
-void AnimationManager::RenderAnimation(int id, Vector2D vec)
-{
-	animations[id]->Render(vec);
-}
-
 AnimationManager::AnimationManager()
 {
 	SpriteManager* spriteManager = SpriteManager::GetInstance();
 
-	// Build Mario run to right animation
+	// Build big Mario run to right animation
 	LPANIMATION ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10001));
 	ani->Add(spriteManager->GetSprite(10002));
 	ani->Add(spriteManager->GetSprite(10003));
 	animations[ANI_MARIO_BIG_RUN_RIGHT] = ani;
 
-	// Build Mario run to left animation
+	// Build small Mario run to right animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10011));
+	ani->Add(spriteManager->GetSprite(10012));
+	ani->Add(spriteManager->GetSprite(10013));
+	animations[ANI_MARIO_SMALL_RUN_RIGHT] = ani;
+
+	// Build big Mario run to left animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10004));
 	ani->Add(spriteManager->GetSprite(10005));
 	ani->Add(spriteManager->GetSprite(10006));
 	animations[ANI_MARIO_BIG_RUN_LEFT] = ani;
 
-	// Build Mario idle to right animation
+	// Build small Mario run to left animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10014));
+	ani->Add(spriteManager->GetSprite(10015));
+	ani->Add(spriteManager->GetSprite(10016));
+	animations[ANI_MARIO_SMALL_RUN_LEFT] = ani;
+
+	// Build big Mario idle to right animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10001));
 	animations[ANI_MARIO_BIG_IDLE_RIGHT] = ani;
 
-	//Build Mario idle to left animation
+	// Build small Mario idle to right animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10011));
+	animations[ANI_MARIO_SMALL_IDLE_RIGHT] = ani;
+
+	//Build big Mario idle to left animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10004));
 	animations[ANI_MARIO_BIG_IDLE_LEFT] = ani;
 
-	//Build Mario jump to right animation
+	//Build small Mario idle to left animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10014));
+	animations[ANI_MARIO_SMALL_IDLE_LEFT] = ani;
+
+	//Build big Mario jump to right animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10007));
 	animations[ANI_MARIO_BIG_JUMP_RIGHT] = ani;
 
-	//Build Mario jump to left animation
+	//Build small Mario jump to right animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10017));
+	animations[ANI_MARIO_SMALL_JUMP_RIGHT] = ani;
+
+	//Build big Mario jump to left animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10008));
 	animations[ANI_MARIO_BIG_JUMP_LEFT] = ani;
 
-	//Build Mario fall to right animation
+	//Build small Mario jump to left animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10018));
+	animations[ANI_MARIO_SMALL_JUMP_LEFT] = ani;
+
+	//Build big Mario fall to right animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10003));
 	animations[ANI_MARIO_BIG_FALL_RIGHT] = ani;
 
-	//Build Mario fall to left animation
+	//Build small Mario fall to right animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10013));
+	animations[ANI_MARIO_SMALL_FALL_RIGHT] = ani;
+
+	//Build big Mario fall to left animation
 	ani = new Animation(ANI_DEFAULT_TIME);
 	ani->Add(spriteManager->GetSprite(10006));
 	animations[ANI_MARIO_BIG_FALL_LEFT] = ani;
+
+	//Build small Mario fall to left animation
+	ani = new Animation(ANI_DEFAULT_TIME);
+	ani->Add(spriteManager->GetSprite(10016));
+	animations[ANI_MARIO_SMALL_FALL_LEFT] = ani;
 
 	// Big Mario switch to right
 	ani = new Animation(300);
 	ani->Add(spriteManager->GetSprite(10009));
 	animations[ANI_MARIO_BIG_SWITCH_RIGHT] = ani;
 
+	// Small Mario switch to right
+	ani = new Animation(300);
+	ani->Add(spriteManager->GetSprite(10019));
+	animations[ANI_MARIO_SMALL_SWITCH_RIGHT] = ani;
+
 	// Big Mario switch to right
 	ani = new Animation(300);
 	ani->Add(spriteManager->GetSprite(10010));
 	animations[ANI_MARIO_BIG_SWITCH_LEFT] = ani;
+
+	// Small Mario switch to right
+	ani = new Animation(300);
+	ani->Add(spriteManager->GetSprite(10020));
+	animations[ANI_MARIO_SMALL_SWITCH_LEFT] = ani;
 
 	//Build background animation
 	ani = new Animation(ANI_DEFAULT_TIME);
@@ -89,4 +133,6 @@ AnimationManager::AnimationManager()
 	ani->Add(spriteManager->GetSprite(10301));
 	ani->Add(spriteManager->GetSprite(10302));
 	animations[ANI_GOOMBA_RUN] = ani;
+
+	DebugOut(L"[INFO] Animations loaded Ok.\n");
 }
--- SuperMario/GameObjectManager.cpp
+++ SuperMario/GameObjectManager.cpp
@@ -4,11 +4,37 @@ GameObjectManager* GameObjectManager::instance = NULL;
 
 GameObjectManager::GameObjectManager()
 {
-    GameObject* obj = new Mario(200, 150);
+    AnimationManager* am = AnimationManager::GetInstance();
+
+    GameObject* obj = new Mario(0, 150);
     obj->SetKeyHandler(Game::GetInstance()->keyHandler);
+    obj->AddAnimation(FIGURE_BIG, STATE_IDLE, LEFT, am->GetAnimation(ANI_MARIO_BIG_IDLE_LEFT));
+    obj->AddAnimation(FIGURE_BIG, STATE_IDLE, RIGHT, am->GetAnimation(ANI_MARIO_BIG_IDLE_RIGHT));
+    obj->AddAnimation(FIGURE_BIG, STATE_RUN, LEFT, am->GetAnimation(ANI_MARIO_BIG_RUN_LEFT));
+    obj->AddAnimation(FIGURE_BIG, STATE_RUN, RIGHT, am->GetAnimation(ANI_MARIO_BIG_RUN_RIGHT));
+    obj->AddAnimation(FIGURE_BIG, STATE_JUMP, LEFT, am->GetAnimation(ANI_MARIO_BIG_JUMP_LEFT));
+    obj->AddAnimation(FIGURE_BIG, STATE_JUMP, RIGHT, am->GetAnimation(ANI_MARIO_BIG_JUMP_RIGHT));
+    obj->AddAnimation(FIGURE_BIG, STATE_FALL, LEFT, am->GetAnimation(ANI_MARIO_BIG_FALL_LEFT));
+    obj->AddAnimation(FIGURE_BIG, STATE_FALL, RIGHT, am->GetAnimation(ANI_MARIO_BIG_FALL_RIGHT));
+    obj->AddAnimation(FIGURE_BIG, SWITCH, LEFT, am->GetAnimation(ANI_MARIO_BIG_SWITCH_LEFT));
+    obj->AddAnimation(FIGURE_BIG, SWITCH, RIGHT, am->GetAnimation(ANI_MARIO_BIG_SWITCH_RIGHT));
+
+    obj->AddAnimation(FIGURE_SMALL, STATE_IDLE, LEFT, am->GetAnimation(ANI_MARIO_SMALL_IDLE_LEFT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_IDLE, RIGHT, am->GetAnimation(ANI_MARIO_SMALL_IDLE_RIGHT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_RUN, LEFT, am->GetAnimation(ANI_MARIO_SMALL_RUN_LEFT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_RUN, RIGHT, am->GetAnimation(ANI_MARIO_SMALL_RUN_RIGHT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_JUMP, LEFT, am->GetAnimation(ANI_MARIO_SMALL_JUMP_LEFT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_JUMP, RIGHT, am->GetAnimation(ANI_MARIO_SMALL_JUMP_RIGHT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_FALL, LEFT, am->GetAnimation(ANI_MARIO_SMALL_FALL_LEFT));
+    obj->AddAnimation(FIGURE_SMALL, STATE_FALL, RIGHT, am->GetAnimation(ANI_MARIO_SMALL_FALL_RIGHT));
+    obj->AddAnimation(FIGURE_SMALL, SWITCH, LEFT, am->GetAnimation(ANI_MARIO_SMALL_SWITCH_LEFT));
+    obj->AddAnimation(FIGURE_SMALL, SWITCH, RIGHT, am->GetAnimation(ANI_MARIO_SMALL_SWITCH_RIGHT));
     gameObjects[ID_MARIO] = obj;
-    obj = new Goomba(224, 161, 0);
+
+    obj = new Goomba(224, 161, 5);
+    obj->AddAnimation(NULL, STATE_RUN, NULL, am->GetAnimation(ANI_GOOMBA_RUN));
     gameObjects[ID_GOOMBA_1] = obj;
+    
     camera = Camera::GetInstance();
 }
 
